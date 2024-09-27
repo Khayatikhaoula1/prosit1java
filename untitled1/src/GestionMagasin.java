0d4e6408ce2206d7c02204d8a15c2f8776b976d4
khayatikhaoula1 <khaoula.khayati@esprit.tn>
2024-09-27 15:44:55 +0100

    prosit 2

--- untitled1/src/GestionMagasin.java
+++ untitled1/src/GestionMagasin.java
@@ -14,5 +14,23 @@ public class GestionMagasin {
             produit.afficher();
             System.out.println();
         }
+        Magasin magasin1 = new Magasin(1, "MG1", 50);
+        Magasin magasin2 = new Magasin(2, "MG2", 50);
 
-    }}
+        // Ajout de produits au magasin 1
+        magasin1.ajouterProduit(new Produit(1021, "Lait", "Delice", 0.7));
+        magasin1.ajouterProduit(new Produit(2510, "Yaourt", "Vitalait", 1.2));
+        magasin1.ajouterProduit(new Produit(3250, "Tomate", "Sicam", 1.3));
+
+        // Ajout de produits au magasin 2
+        magasin2.ajouterProduit(new Produit(1051, "Chocolat", "MARQUEPROD2", 3.0));
+        magasin2.ajouterProduit(new Produit(2061, "Jus", "MARQUEPROD1", 1.5));
+
+        magasin1.afficherMagasin();
+        System.out.println();
+        magasin2.afficherMagasin();
+
+        int totalProduits = magasin1.getNombreProduits() + magasin2.getNombreProduits();
+        System.out.println("\nNombre total de produits dans tous les magasins : " + totalProduits);
+    }
+    }
